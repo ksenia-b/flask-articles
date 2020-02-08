9d63a0374b00e8e257cdc05477024ab439d69c07
Oksana <ksenia.barshchyk@yandex.ru>
2020-02-08 14:41:51 +0200

    added navbar, article and about pages

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article 1",
+            "body": "Lorem ipsum Lorem ipsum",
+            "author": "Oksana B.",
+            "created_date": "04.12.1995"
+        },
+        {
+            "id": 2,
+            "title": "Article 2",
+            "body": "Lorem ipsum Lorem ipsum Lorem ipsum",
+            "author": "Oksana B.",
+            "created_date": "04.12.1995"
+        },
+        {
+            "id": 3,
+            "title": "Article 3",
+            "body": "Lorem ipsum",
+            "author": "Oksana B.",
+            "created_date": "04.12.1995"
+        }
+
+    ]
+    return articles
